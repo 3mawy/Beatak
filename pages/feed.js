f0327214fe5f7c25f024caa5446d3f575cfd62b0
3mawy <m.3mawyy@gmail.com>
2022-03-07 02:12:40 +0200

    Edit Feed rtl layout

--- pages/feed.js
+++ pages/feed.js
@@ -6,7 +6,7 @@ function Feed(props) {
     return (
         <div className={`xl:container grid mx-auto px-4`}>
 
-            <div className={`grid md:space-x-3 grid-cols-1 md:grid-cols-3 lg:grid-cols-8 pt-11`}>
+            <div className={`grid md:space-s-3 grid-cols-1 md:grid-cols-3 lg:grid-cols-8 pt-11`}>
                 <div className="hidden md:block md:col-span-1 lg:col-span-2"><FeedCard data={`Tags`}/></div>
                 <div className="col-start-1  md:col-span-2 lg:col-span-4"><FeedCard data={`Feed`}/></div>
                 <div className="col-span-2 hidden lg:block"><FeedCard data={`Suggested`}/></div>
